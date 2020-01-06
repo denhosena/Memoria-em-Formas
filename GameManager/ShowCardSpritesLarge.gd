6edc07484f7b8e81f6fdfdc6d9e5b2e13ab5bcc7
Ronaldo Sena Junior <denho_sena@hotmail.com>
2020-01-06 20:47:59 -0300

    Basic Animations
    Feito uma animação placeholder para ter ideia de como vai se comportar a carta quando for igual a outra.

--- GameManager/ShowCardSpritesLarge.gd
+++ GameManager/ShowCardSpritesLarge.gd
@@ -3,9 +3,10 @@ class_name ShowCardSpriteLarge
 
 onready var sprite1 = $Sprite
 onready var sprite2 = $Sprite2
+onready var animator = $AnimationPlayer
 
 func _ready():
-	hide()
+	self.modulate = 0
 	pass
 
 
@@ -14,12 +15,25 @@ func Set_Sprites(var spr1, var spr2):
 	sprite2.texture = spr2
 
 func Show_Card_Sprites():
-	show()
-	$Timer.wait_time = 1
-	$Timer.start()
+	animator.play("FadeIn")
+
 
 func Hide_Card_Sprite():
-	hide()
+	animator.play("FadeOut")
 
-func _on_Timer_timeout():
-	Hide_Card_Sprite()
+func _on_Button_pressed():
+	if animator.current_animation == "FadeIn":
+		var pos = animator.current_animation_length
+		animator.advance(pos)
+	if animator.current_animation == "FadeOut":
+		var pos = animator.current_animation_length
+		animator.advance(pos)
+	if animator.current_animation == "Idle":
+		Hide_Card_Sprite()
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if anim_name == "FadeIn":
+		animator.play("Idle")
+	if anim_name == "Idle":
+		Hide_Card_Sprite()
