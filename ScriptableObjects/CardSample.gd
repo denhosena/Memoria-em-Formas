6811cd2eaf6f51acb55df1e97293c2e88b849572
denhosena <denho_sena@hotmail.com>
2020-01-12 15:21:41 -0400

    Adicionado o botao de checkbox para dar autoscroll no texto.

--- ScriptableObjects/CardSample.gd
+++ ScriptableObjects/CardSample.gd
@@ -7,9 +7,15 @@ onready var profile_image = get_node("profile_image")
 onready var card_name = get_node("card_name")
 onready var card_description = get_node("card_description")
 
+var auto_scroll = false
 
 func _ready():
 	set_data()
+	pass
+
+func _process(delta):
+	if auto_scroll:
+		Auto_scroll(delta)
 	pass
 
 func set_data():
@@ -19,13 +25,12 @@ func set_data():
 	card_description.append_bbcode(description)
 	pass
 
-func auto_scroll():
+func Auto_scroll(delta):
 	var scroll_to = card_description.get_v_scroll()
-	scroll_to.value = lerp(scroll_to.value, scroll_to.max_value, 0.02)
-	print(scroll_to.value)
-	
+	if (scroll_to.value == 0):
+		yield(get_tree().create_timer(0.5),"timeout")
+	scroll_to.value += 30 * delta
 
-func _on_Timer_timeout():
-	auto_scroll()
-	$Timer.wait_time = 1;
-	$Timer.start()
+func _on_CheckBox_toggled(button_pressed):
+	auto_scroll = button_pressed
+	pass # Replace with function body.
