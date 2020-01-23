48bb7b594d0de0cbf77a3bbb157d11611fcdcd56
Ronaldo Sena Junior <denho_sena@hotmail.com>
2020-01-22 22:35:29 -0300

    merge
    merge the linux to windows

--- ScriptableObjects/CardSample.gd
+++ ScriptableObjects/CardSample.gd
@@ -7,9 +7,16 @@ onready var profile_image = get_node("profile_image")
 onready var card_name = get_node("card_name")
 onready var card_description = get_node("card_description")
 
+var toggle
+var can_scroll = true
 
 func _ready():
 	set_data()
+	pass
+
+func _process(delta):
+	if (toggle):
+		auto_scroll(delta)
 	pass
 
 func set_data():
@@ -19,13 +26,13 @@ func set_data():
 	card_description.append_bbcode(description)
 	pass
 
-func auto_scroll():
-	var scroll_to = card_description.get_v_scroll()
-	scroll_to.value = lerp(scroll_to.value, scroll_to.max_value, 0.02)
-	print(scroll_to.value)
-	
+func auto_scroll(delta):
+	var scroll = card_description.get_v_scroll()
+	var scroll_value = 0.5
+	scroll.value += scroll_value
 
-func _on_Timer_timeout():
-	auto_scroll()
-	$Timer.wait_time = 1;
-	$Timer.start()
+
+
+func _on_CheckButton_toggled(button_pressed):
+	toggle = button_pressed
+	pass # Replace with function body.
